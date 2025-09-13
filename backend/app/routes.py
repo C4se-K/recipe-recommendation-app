7b2dc57458785a1a6fa5531e99e76ce8820be8c3
kimi_no_shiriai <94500383+C4se-K@users.noreply.github.com>
2025-09-12 17:54:56 -0700

    backend

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -2,13 +2,27 @@ from fastapi import APIRouter, Query
 import requests
 from .models import Feedback
 #from
+import jsonify
 
 router = APIRouter(prefix="/api.recipies")
 
 @router.get("/")
-def get_recipies():
+def status():
+    return {'status':'OK'}
 
-    return
+@router.get("/recipe")
+def get_recipies(query: str = Query(None), exclude: str = Query(None)):
+    url = f"https://www.themealdb.com/api/json/v1/1/search.php?s={query or ''}"
+    if not query:
+        url = f"https://www.themealdb.com/api/json/v1/1/random.php"
+
+    response = requests.get(url)
+    recipes = response.json().get('meals', [])
+
+    if exclude:
+        recipes = [meal for meal in recipes if not any(exclude.lower() in val.lower() for val in meal.values()if val)]
+
+    return recipes
 
 
 @router.post("/feedback")
